74ec27a3c6a5ee304cc3df4c6f522c858a1f313b
snigdhadeep <snigdhadeep93@gmail.com>
2018-11-25 18:05:11 +0530

    first comment

---
+++ app/src/main/java/com/techpenta/uploadfirebase/MainActivity.java
@@ -0,0 +1,138 @@
+package com.techpenta.uploadfirebase;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int PICK_IMAGE_REQUEST =123 ;
+    ImageView image;
+    Button btnchoose;
+    Button btnupload;
+
+    private Uri filepath;
+    private StorageReference mStorageRef;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        image=(ImageView)findViewById(R.id.thumbnail);
+        btnchoose=(Button)findViewById(R.id.btnchooseimage);
+        btnupload=(Button)findViewById(R.id.btnupload);
+
+        btnchoose.setOnClickListener(this);
+        btnupload.setOnClickListener(this);
+
+
+    }
+    private void showfilechooser(){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"select an image"),PICK_IMAGE_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+
+            filepath=data.getData();
+            try {
+                Bitmap bitmap=MediaStore.Images.Media.getBitmap(getContentResolver(),filepath);
+                image.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+    private  void uploadFile(){
+
+        if (filepath!=null) {
+
+            StorageReference riversRef = mStorageRef.child("images/check.jpg");
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading.....");
+            progressDialog.show();
+
+            riversRef.putFile(filepath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "file is uploaded", Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            // ...
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, ""+exception.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage(((int) progress) + "% uploaded....");
+                        }
+                    })
+
+            ;
+        }else {
+
+            Toast.makeText(this, "select an image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==btnchoose){
+         //choose image from gallery
+
+
+         showfilechooser();
+
+
+        }
+        else if (v==btnupload) {
+
+            uploadFile();
+        }
+        }
+
+}
